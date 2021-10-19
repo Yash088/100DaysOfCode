a7458ee711e4033a6d552ed7cdd6e635dc573e89
Yash088 <43039778+Yash088@users.noreply.github.com>
2021-10-19 11:49:30 +0530

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,5 +1,6 @@
 # we have choose index for min not the value like min = arr[i] because we have to swap the index and if choose min than we can't find the jth index from whihch we hvae to replace
 # because at every iteration of i j we are moving till the end of the array. one other can be to have another variable that store j index of the minest value 
+# The good thing about selection sort is it never makes more than O(n) swaps and can be useful when memory write is a costly operation. 
 arr = [64, 25, 12, 22, 11]
 n = len(arr)
 for i in range(n):
