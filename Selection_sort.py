b61277b4974448fde6521059f06e30df7b845379
Yash088 <43039778+Yash088@users.noreply.github.com>
2021-10-19 11:48:41 +0530

    Create Selection_sort.py

---
+++ Selection_sort.py
@@ -0,0 +1,19 @@
+# we have choose index for min not the value like min = arr[i] because we have to swap the index and if choose min than we can't find the jth index from whihch we hvae to replace
+# because at every iteration of i j we are moving till the end of the array. one other can be to have another variable that store j index of the minest value 
+arr = [64, 25, 12, 22, 11]
+n = len(arr)
+for i in range(n):
+    min = i
+#   min = arr[i]
+#   min_j = -1
+    for j in range(i+1,n):
+        if (arr[j] <  arr[min] ):
+#       if (arr[j] <  min ):
+            min = j
+#           min = arr[j]
+#           min_j = j
+    arr[i], arr[min] = arr[min], arr[i]
+#        arr[i], arr[min_j] = arr[min_j], arr[i]
+
+
+print(arr)
