563c46fa2e90d3d43af935f24ac6d3654d427dea
Yash088 <43039778+Yash088@users.noreply.github.com>
2021-10-20 11:59:16 +0530

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,5 @@
 # Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
+# Write Python 3 code in this online editor and run it.TIme Complexity:- O(N^2). we can use binary ssearch in place of linear search. 
 # insertion sort start with 1 ... len(arr) because in the insertion sort you have one sorted array for traverse. 
 # get the key and go till i-1 to 0 till the element of the array is greater than the current key so that you can get the array index to insert
 # while traversing backward you have to swap the element so that you can insert at the index as we have done in the line no. 9. than you have the index insert at the j+1 and Bingo :)
