ee26054dbc1684bb3e635850b1b775a2bacf99b3
Yash088 <43039778+Yash088@users.noreply.github.com>
2021-10-22 11:02:02 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -135,3 +135,29 @@ def subset(n):
         
 
 print(subset(4))
+# Permutation
+# T.C.:- O(N*N!)
+# S.C:- O(r-l).
+
+def toString(List):
+    return ''.join(List)
+ 
+# Function to print permutations of string
+# This function takes three parameters:
+# 1. String
+# 2. Starting index of the string
+# 3. Ending index of the string.
+def permute(a, l, r):
+    if l==r:
+        print(toString(a))
+    else:
+        for i in range(l,r+1):
+            a[l], a[i] = a[i], a[l]
+            permute(a, l+1, r)
+            a[l], a[i] = a[i], a[l] # backtrack
+ 
+# Driver program to test the above function
+string = "ABC"
+n = len(string)
+a = list(string)
+permute(a, 0, n-1)
